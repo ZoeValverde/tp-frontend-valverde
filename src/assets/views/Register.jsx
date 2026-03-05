2f5a124bf795f76a0d2479d64db9bb6986109805
zoevalverde <zoevalverde00@gmail.com>
2026-03-05 16:41:57 -0300

    add: funtion validation Register

--- src/assets/views/Register.jsx
+++ src/assets/views/Register.jsx
@@ -7,17 +7,15 @@ const Register = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [name, setName] = useState("")
-  const navigate = useNavigate()
+  const [errorName, setErrorName] = useState(null)
+  const [errorPassword, setErrorPassword]=useState(null)
   const {setLoggedUser} = useContext(ChatContext)
+  const navigate = useNavigate()
   
   const handleNavigate= ()=> {
    navigate("/Login")
   }
 
-  const handleRegister = () => {
-    setLoggedUser(true)
-    navigate("/")
-}
   const { handleAccounts } = useContext(ChatContext)
     
  const handleChangeEmail = (e) => {
@@ -33,13 +31,18 @@ const Register = () => {
   }
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        
-        if(!email || !password || !name){return}
-
-    
-      handleAccounts({ email, name, password })
+      e.preventDefault()
+      if (!email || !password || !name) { return }
       
+      
+      if (name.length < 3 || (password.length < 6)  ) { 
+        setErrorName(true)
+        setErrorPassword(true)
+        return
+      }
+    
+    handleAccounts({ email, name, password })  
+    setLoggedUser(true)
     navigate("/")
   }
 
@@ -51,21 +54,31 @@ const Register = () => {
           type="email"
           placeholder="correo electronico"
           value={email}
+          required
           onChange={handleChangeEmail}
         />
         <input
           type="text"
           placeholder="nombre de usuario"
           value={name}
+          required
           onChange={handleChangeName}
         />
         <input
           type="password"
           placeholder="contraseña"
           value={password}
+          required
           onChange={handleChangePassword}
         />
-        <button onClick={handleRegister}>INGRESAR</button>
+        {
+          errorName && <p>El nombre de usuario requiere minimo 3 caracteres</p>
+        }
+        {
+          errorPassword && <p>La contraseña requiere minimo 6 caracteres</p>
+        }
+        <button onClick={handleSubmit}>INGRESAR</button>
+
           </form>
           <h2>¿Ya tienes una cuenta?</h2>
       <button onClick={handleNavigate}>Inicia Sesion aquí</button>
